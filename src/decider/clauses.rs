a9f093669d85d06b17a95db886b81fddb500d495
Columbus240 <8899730+Columbus240@users.noreply.github.com>
2022-09-03 16:50:56 +0200

    Move the cut rule to `clauses.rs`
    This simplifies the view from `unifiability.rs`

--- src/decider/clauses.rs
+++ src/decider/clauses.rs
@@ -936,7 +936,11 @@ impl ClauseIrred {
     pub fn unifiability_simplify(self) -> ClauseWaitingConj {
         let mut clause_irred = self.simplify();
         clause_irred.unifiability_simplify_free_atoms();
-        let mut clause: ClauseWaitingConj = clause_irred.into();
+        let mut clause: ClauseWaitingConj =
+            match clause_irred.unifiability_simplify_perform_cut_rule() {
+                Ok(c) => c.into(),
+                Err(c) => c.into(),
+            };
         clause.unifiability_simplify();
         clause
     }
@@ -992,6 +996,100 @@ impl ClauseIrred {
 
     pub fn display_spartacus(&self) -> ClauseIrredDisplaySpartacus {
         ClauseIrredDisplaySpartacus { clause: self }
+    }
+
+    fn unifiability_simplify_perform_cut_rule(mut self) -> Result<ClauseIrred, ClauseAtoms> {
+        // For all sequents `sequent`, for all variables `p` on the left of this sequent,
+        // Search for other sequents with `p` on the right. Then perform
+        // cut on these two sequents and this variable. Add the resulting sequent to a waiting list.
+        // In the end add the waiting list to the `clause` and return.
+
+        let mut new_irred_sequents: BTreeSet<PSI> = BTreeSet::new();
+        let mut new_box_sequents: Vec<PSB> = Vec::new();
+
+        for left_sequent in self.irreducibles.iter() {
+            // iterate over all the left atoms of `sequent`
+            for i in left_sequent
+                .atoms
+                .iter()
+                .filter_map(|(i, left_right)| match left_right {
+                    LeftRight::Left => Some(i),
+                    LeftRight::Right => None,
+                })
+            {
+                // now find all sequents with `i` on the right
+                for right_sequent in self.irreducibles.iter() {
+                    if right_sequent.atoms.get(i) != Some(&LeftRight::Right) {
+                        continue;
+                    }
+                    // create the new sequent as a combination of
+                    // `left_sequent` and `right_sequent` where `i` is left out.
+                    // And if an atom appears both left and right, then the new
+                    // sequent becomes trivial, so skip that.
+
+                    // The new sets of atoms
+                    let mut new_atoms = left_sequent.atoms.clone();
+                    new_atoms.remove(i);
+
+                    for (j, lr) in right_sequent.atoms.iter() {
+                        match new_atoms.insert(*j, *lr) {
+                            None => {}
+                            Some(prev_lr) => {
+                                if prev_lr != *lr {
+                                    // The sequent is trivial
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+
+                    let mut new_lb = left_sequent.lb.clone();
+                    new_lb.append(&mut right_sequent.lb.clone());
+                    let mut new_rb = left_sequent.rb.clone();
+                    new_rb.append(&mut right_sequent.rb.clone());
+
+                    let new_sequent: PSI = PSI {
+                        atoms: new_atoms,
+                        lb: new_lb,
+                        rb: new_rb,
+                    };
+
+                    // Now try transforming the `new_sequent` into a `PSB`.
+                    // If that doesn't work, check whether it is already present in `clause`
+
+                    match new_sequent.try_into() {
+                        Ok(new_sequent_psb) => {
+                            new_box_sequents.push(new_sequent_psb);
+                        }
+                        Err(new_sequent) => {
+                            if !self.irreducibles.contains(&new_sequent) {
+                                new_irred_sequents.insert(new_sequent);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Now return a clause, without loosing any information.
+        let new_irred_sequents_is_empty = new_irred_sequents.is_empty();
+        self.irreducibles.append(&mut new_irred_sequents);
+        if !new_box_sequents.is_empty() {
+            return Err(ClauseAtoms {
+                irreducibles: self.irreducibles,
+                atom_sequents: new_box_sequents,
+            });
+        }
+
+        if new_irred_sequents_is_empty {
+            Ok(self)
+        } else {
+            //TODO: This call is inefficient for two reasons:
+            // - Rust does not do tail-call-optimization, so a loop would be better
+            // - In the next iteration, we have forgotten, which sequents were already matched together.
+            //   This way we do lots of duplicate work.
+            self.unifiability_simplify_perform_cut_rule()
+        }
     }
 }
 
