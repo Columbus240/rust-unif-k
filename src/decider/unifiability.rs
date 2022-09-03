410e8131fc76889085f78167b6eea74bd4a5b24e
Columbus240 <8899730+Columbus240@users.noreply.github.com>
2022-09-03 16:41:01 +0200

    unif: Add the cut rule for atoms.

--- src/decider/unifiability.rs
+++ src/decider/unifiability.rs
@@ -13,6 +13,100 @@ fn clause_waiting_unif_step(mut clause: ClauseWaitingConj) -> ClauseWaitingConj 
     clause.process_conjs_step()
 }
 
+fn clause_irred_perform_cut_rule(mut clause: ClauseIrred) -> Result<ClauseIrred, ClauseAtoms> {
+    // For all sequents `sequent`, for all variables `p` on the left of this sequent,
+    // Search for other sequents with `p` on the right. Then perform
+    // cut on these two sequents and this variable. Add the resulting sequent to a waiting list.
+    // In the end add the waiting list to the `clause` and return.
+
+    let mut new_irred_sequents: BTreeSet<PSI> = BTreeSet::new();
+    let mut new_box_sequents: Vec<PSB> = Vec::new();
+
+    for left_sequent in clause.irreducibles.iter() {
+        // iterate over all the left atoms of `sequent`
+        for i in left_sequent
+            .atoms
+            .iter()
+            .filter_map(|(i, left_right)| match left_right {
+                LeftRight::Left => Some(i),
+                LeftRight::Right => None,
+            })
+        {
+            // now find all sequents with `i` on the right
+            for right_sequent in clause.irreducibles.iter() {
+                if right_sequent.atoms.get(i) != Some(&LeftRight::Right) {
+                    continue;
+                }
+                // create the new sequent as a combination of
+                // `left_sequent` and `right_sequent` where `i` is left out.
+                // And if an atom appears both left and right, then the new
+                // sequent becomes trivial, so skip that.
+
+                // The new sets of atoms
+                let mut new_atoms = left_sequent.atoms.clone();
+                new_atoms.remove(i);
+
+                for (j, lr) in right_sequent.atoms.iter() {
+                    match new_atoms.insert(*j, *lr) {
+                        None => {}
+                        Some(prev_lr) => {
+                            if prev_lr != *lr {
+                                // The sequent is trivial
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                let mut new_lb = left_sequent.lb.clone();
+                new_lb.append(&mut right_sequent.lb.clone());
+                let mut new_rb = left_sequent.rb.clone();
+                new_rb.append(&mut right_sequent.rb.clone());
+
+                let new_sequent: PSI = PSI {
+                    atoms: new_atoms,
+                    lb: new_lb,
+                    rb: new_rb,
+                };
+
+                // Now try transforming the `new_sequent` into a `PSB`.
+                // If that doesn't work, check whether it is already present in `clause`
+
+                match new_sequent.try_into() {
+                    Ok(new_sequent_psb) => {
+                        new_box_sequents.push(new_sequent_psb);
+                    }
+                    Err(new_sequent) => {
+                        if !clause.irreducibles.contains(&new_sequent) {
+                            new_irred_sequents.insert(new_sequent);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Now return a clause, without loosing any information.
+    let new_irred_sequents_is_empty = new_irred_sequents.is_empty();
+    clause.irreducibles.append(&mut new_irred_sequents);
+    if !new_box_sequents.is_empty() {
+        return Err(ClauseAtoms {
+            irreducibles: clause.irreducibles,
+            atom_sequents: new_box_sequents,
+        });
+    }
+
+    if new_irred_sequents_is_empty {
+        Ok(clause)
+    } else {
+        //TODO: This call is inefficient for two reasons:
+        // - Rust does not do tail-call-optimization, so a loop would be better
+        // - In the next iteration, we have forgotten, which sequents were already matched together.
+        //   This way we do lots of duplicate work.
+        clause_irred_perform_cut_rule(clause)
+    }
+}
+
 /// Returns `true` if the clause is unifiable, returns `false` if it is undecided or not unifiable.
 /// All further clauses that are created by this function are stored both in `visited_clauses` and `clause_set`.
 fn check_unifiable_process_conjs(
@@ -77,7 +171,21 @@ fn check_unifiable_process_atoms(
                 ProcessAtomsResult::Valid => true,
                 ProcessAtomsResult::Contradictory => false,
                 ProcessAtomsResult::Irred(clause_irred) => {
-                    clause_set.lock().unwrap().irreducibles.insert(clause_irred);
+                    let mut clause_set = clause_set.lock().unwrap();
+                    let mut visited_clauses = visited_clauses.lock().unwrap();
+                    // Try apply the cut rule to this clause.
+                    match clause_irred_perform_cut_rule(clause_irred) {
+                        Ok(clause_irred) => {
+                            if visited_clauses.insert(clause_irred.clone().into()) {
+                                clause_set.irreducibles.insert(clause_irred);
+                            }
+                        }
+                        Err(clause_atoms) => {
+                            if visited_clauses.insert(clause_atoms.clone().into()) {
+                                clause_set.waiting_atoms.push(clause_atoms);
+                            }
+                        }
+                    }
                     false
                 }
                 ProcessAtomsResult::Clause(clause_next) => {
