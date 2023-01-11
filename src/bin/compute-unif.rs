c7c33de9f1c2370ee11c0244e391edde948ed14d
Columbus240 <8899730+Columbus240@users.noreply.github.com>
2023-01-11 11:32:46 +0100

    compute-unif: Set it up for 2-vars, deg-1
    But note that there are 2^64 such formulas. Some reduction is necessary.

--- src/bin/compute-unif.rs
+++ src/bin/compute-unif.rs
@@ -13,42 +13,44 @@ fn main() {
 
     let mut stuck_clauses: Vec<(usize, ClauseSet)> = Vec::new();
 
-    FineFormNNFIter::new(1)
-        .take(1000)
-        .enumerate()
-        //        .par_bridge()
-        .for_each(|(i, nnf)| {
-            let nnf_simpl = nnf.simpl();
-            let deg = nnf_simpl.degree();
-            let nnf_simpl_unif = nnf_simpl.check_unifiable();
-            //println!("formula: {}", nnf.display_beautiful());
-            match nnf_simpl_unif {
-                Ok(b) => {
-                    if b {
-                        dec_true.inc();
-                    } else {
-                        dec_false.inc();
-                    }
-                    println!("index {}, degree {}, unifiable: {}", i, deg, b);
+    let iter = FineFormNNFIter::new(2).enumerate();
+
+    for (i, nnf) in iter {
+        let nnf_simpl = nnf.simpl();
+        let deg = nnf_simpl.degree();
+        if deg > 1 {
+            println!("degree exceeds 1");
+            break;
+        }
+        let nnf_simpl_unif = nnf_simpl.check_unifiable();
+        //println!("formula: {}", nnf.display_beautiful());
+        match nnf_simpl_unif {
+            Ok(b) => {
+                if b {
+                    dec_true.inc();
+                } else {
+                    dec_false.inc();
                 }
-                Err(e) => {
-                    undecidable.inc();
-                    println!(
-                        "index {}, degree {}, gets stuck at:\n{}",
-                        i,
-                        deg,
-                        e.display_beautiful()
-                    );
-                    stuck_clauses.push((i, e));
-                }
+                println!("index {}, degree {}, unifiable: {}", i, deg, b);
             }
-            println!(
-                "stuck {} vs. dec true {} dec false {}",
-                undecidable.get(),
-                dec_true.get(),
-                dec_false.get()
-            );
-        });
+            Err(e) => {
+                undecidable.inc();
+                println!(
+                    "index {}, degree {}, gets stuck at:\n{}",
+                    i,
+                    deg,
+                    e.display_beautiful()
+                );
+                stuck_clauses.push((i, e));
+            }
+        }
+        println!(
+            "stuck {} vs. dec true {} dec false {}",
+            undecidable.get(),
+            dec_true.get(),
+            dec_false.get()
+        );
+    }
 
     // texify stuck clauses
     println!("\\begin{{tabular}}{{cl}}");
