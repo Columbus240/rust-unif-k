485c523b5705ca8e1dc23061d5868018ed07b88a
Columbus240 <8899730+Columbus240@users.noreply.github.com>
2023-02-17 11:01:20 +0100

    bin/print-fineform: fix termination condition
    Before this patch, it would always output the first formula of the next
    level.

--- src/bin/print-fineform.rs
+++ src/bin/print-fineform.rs
@@ -4,12 +4,16 @@ use generator::BasicFineFormNNFIter;
 use generator::NNF;
 
 fn main() {
-    let num_variables = 1;
+    let num_variables = 0;
     let mut iter_ff = BasicFineFormIter::new(num_variables);
 
     let mut i: usize = 0;
     loop {
         let ff = iter_ff.next().unwrap();
+        if iter_ff.get_curr_level() > 2 {
+            break;
+        }
+
         println!(
             "index {}, level_ff {}, ff {}",
             i,
@@ -17,9 +21,5 @@ fn main() {
             ff
         );
         i += 1;
-
-	if iter_ff.get_curr_level() > 2 {
-		break;
-	}
     }
 }
